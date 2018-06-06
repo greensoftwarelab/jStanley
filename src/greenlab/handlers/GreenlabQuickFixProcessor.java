11d595f20b67b3141460919756921de2f497ed93
pedrosimao <psimao93@gmail.com>
2018-06-06 20:33:39 +0100

    bug fixes

--- src/greenlab/handlers/GreenlabQuickFixProcessor.java
+++ src/greenlab/handlers/GreenlabQuickFixProcessor.java
@@ -2,10 +2,14 @@ package greenlab.handlers;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.jdt.ui.text.java.IInvocationContext;
 import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
 import org.eclipse.jdt.ui.text.java.IProblemLocation;
 import org.eclipse.jdt.ui.text.java.IQuickFixProcessor;
+import org.eclipse.jdt.ui.text.java.correction.ASTRewriteCorrectionProposal;
 
 public class GreenlabQuickFixProcessor implements IQuickFixProcessor{
 
@@ -21,7 +25,15 @@ public class GreenlabQuickFixProcessor implements IQuickFixProcessor{
 			return null;
 		}
 		
-		return null;
+		AST ast = context.getASTRoot().getAST();
+		ASTRewrite rw = ASTRewrite.create(ast);
+		ASTNode node = ast.newSimpleName("Test");
+		rw.replace(context.getCoveringNode(), node, null);
+		
+		System.out.println("covering" + context.getCoveringNode().toString());
+
+		IJavaCompletionProposal p = new ASTRewriteCorrectionProposal("Change LinkedList by ArrayList", context.getCompilationUnit(), rw, 100);
+		return new IJavaCompletionProposal[] {p};
 	}
 
 }
